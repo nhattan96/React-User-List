9c8548f38536610132db1dd2ddfa2b126f5d9e91
nhattan96 <tranhuynhnhattan96@gmail.com>
2022-04-14 23:09:21 +0700

    update

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,26 +2,69 @@ import React from 'react'
 import Button from './Button'
 import Card from './Card'
 import styles from './ErrorModel.module.css'
+import ReactDOM from 'react-dom'
+
+const Backdrop = props => {
+    return <div className={styles.backdrop} onClick={props.onConfirm}></div>
+}
+
+const ModalOverlay = props => {
+    return (<Card className={styles.modal}>
+        <header className={styles.header}>
+            <h2>
+                {props.title}
+            </h2>
+        </header>
+        <div className={styles.content}>
+            <p className={styles.message}>
+                {props.message}
+            </p>
+        </div>
+        <footer className={styles.actions}>
+            <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+    </Card>)
+}
 
 const ErrorModal = (props) => {
     return (
-        <div className={styles.backdrop} onClick={props.onConfirm}>
+        <>
+            {/* <div className={styles.backdrop} onClick={props.onConfirm}></div>
             <Card className={styles.modal}>
-            <header className={styles.header}>
-                <h2>
-                    {props.title}
-                </h2>
-            </header>
-            <div className={styles.content}>
-                <p className={styles.message}>
-                    {props.message}
-                </p>
-            </div>
-            <footer className={styles.actions}>
-                <Button onClick={props.onConfirm}>Okay</Button>
-            </footer>
-        </Card>
-        </div>
+                <header className={styles.header}>
+                    <h2>
+                        {props.title}
+                    </h2>
+                </header>
+                <div className={styles.content}>
+                    <p className={styles.message}>
+                        {props.message}
+                    </p>
+                </div>
+                <footer className={styles.actions}>
+                    <Button onClick={props.onConfirm}>Okay</Button>
+                </footer>
+            </Card> */}
+
+            {/* Use React Portal */}
+
+            {
+                ReactDOM.createPortal(
+                    <Backdrop onConfirm={props.onConfirm}></Backdrop>,
+                    document.getElementById('backdrop-root')
+                )
+            }
+            {
+                ReactDOM.createPortal(
+                    <ModalOverlay
+                        title={props.title}
+                        message={props.message}
+                        onConfirm={props.onConfirm}
+                    ></ModalOverlay>,
+                    document.getElementById('overlay-root')
+                )
+            }
+        </>
     )
 }
 
